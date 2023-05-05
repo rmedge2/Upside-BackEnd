93446dc47e8875a63d96a4d13b2ba1f5a3582bec
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-05-05 15:09:35 -0400

    get request available for therapists

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const therapistsRouter = require('./routes/therapists')
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/therapists', therapistsRouter);
 
 module.exports = app;
